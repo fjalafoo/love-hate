bae182b80371bc78f74ad731cba2091dbec6049a
fjalafoo <fjalafoo@gmail.com>
2022-10-16 10:14:26 +0300

    favorite animals

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 const express = require('express')
 const app = express()
-
+const ejsLayouts = require('express-ejs-layouts')
 const PORT = 3000
 //MIDDLEWARE
 app.set('view engine', 'ejs')
+app.use(ejsLayouts)
 
 //ROUTES
+
+//home
+app.get('/',(req,res)=>{
+    res.render('home.ejs')
+})
+
+//fav animals
+app.get('/animals', (req,res)=>{
+    const favAnimals=['bunnies','dogs','cats','horses']
+    res.render('animals.ejs',{animals:favAnimals})
+})
+
+
 
 app.listen(PORT, ()=>{
     console.log(`You're running ahbeh-akerha app.`)
